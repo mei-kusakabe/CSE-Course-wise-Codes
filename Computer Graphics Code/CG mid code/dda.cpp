9dcbc95aadc8e58a8245eb36e67d74cebd50771a
Jafrin Iqbal Chowdhury <71540089+mei-kusakabe@users.noreply.github.com>
2021-10-12 11:53:50 +0600

    Add files via upload

---
+++ Computer Graphics Code/CG mid code/dda.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#include <graphics.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+
+    initwindow(1920,1080, "DDA Algorithm implementation");
+    setbkcolor(15);
+    cleardevice();
+
+    double x1,x2,y1,y2,m,b,x,y;
+    ll x1_p,x2_p,y1_p,y2_p;
+    cin>>x1>>y1>>x2>>y2;
+    x1_p = floor(x1);
+    y1_p = floor(y1);
+    x2_p = floor(x2);
+    y2_p = floor(y2);
+    m= (y2_p -y1_p)/(x2_p-x1_p);
+
+
+    if(abs(m)<=1)
+    {
+        y=y1_p;
+        for(ll i = x1_p; i<=x2_p ; i++)
+        {
+            y+=m; ;
+            putpixel (i, y, RED);
+        }
+    }
+    else
+    {
+        x= x1_p;
+        for(ll i = y1_p; i<=y2_p ; i++)
+        {
+            x +=(1/m);
+            putpixel (x, i, RED);
+        }
+    }
+    getch();
+    closegraph();
+
+    return 0;
+
+}
+
